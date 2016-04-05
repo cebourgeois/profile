8e753d987b235699769235dd44dd1ec9c1b5d9f0
cebourgeois <cedricbourg@gmail.com>
2016-04-05 18:57:35 +0200

    Adding pages

---
+++ dynamique.js
@@ -0,0 +1,20 @@
+//$(".copyright").hide();
+
+
+
+/* $(document).ready(function(){
+	$(".copyright p").show();	
+});
+*/
+
+/*
+	$(function(){
+		$(".copyright p").css("color", "blue");
+	});
+*/
+
+	$(document).ready(function(){
+		$("#test").click(function(){
+			$
+		});
+	});
